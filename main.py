20f840922691b55d503b8b68fa8f3af09aa77514
JWaltzzz <jermaine@wouessi.com>
2025-11-21 02:35:27 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_agent
+from tools import scrape_menu
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+
+query = input("What would you like to compare? ")
+# response = llm.invoke(query)
+# print(response)
+
+system_prompt = """
+You are a price comparison assistant.
+
+I need you to scrape a restaurant webpage (that the user provides a URL to) using the `scrape_menu` tool. 
+
+
+Given a user request, you may:
+- call scrape_menu(url) to get the entire menu from the URL.
+- From the returned response of scrape_menu, analyze all the items and prices.
+- Determine the cheapest main dish + drink combination available. An appetizer or kids menu item does not count as a main dish.
+- Return to the user the name of the restaurant, the cheapest main dish + drink combination and the total price in this format: 
+  restaurant name: main dish ($price) + drink ($price) -> Total: $total_price
+- If a drink is included with the main dish, mention it in your response, e.g. "main dish (includes drink)".
+
+"""
+
+tools = [scrape_menu]
+
+agent = create_agent(
+    model=llm,
+    tools=tools,
+    system_prompt=system_prompt,
+)
+
+result_state = agent.invoke(
+    {
+        "messages": [
+            {"role": "user", "content" : query}
+        ]
+    }
+)
+
+returned_response = result_state["messages"][-1].content
+if isinstance(returned_response, list):
+    returned_response = "".join([x.get("text", "") if isinstance(x, dict) else str(x) for x in returned_response])
+
+print("\n---- Agent Response ----")
+print(returned_response)
+
+
---
+++ tools.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+from langchain_community.document_loaders import AsyncChromiumLoader
+from langchain_community.document_transformers import Html2TextTransformer
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.tools import tool
+
+@tool
+def scrape_menu(url: str) -> str:
+    """"Scrape the menu from the restaurant URL and follow the steps below to return the text content"""
+    # 1. Load and convert HTML → text
+    loader = AsyncChromiumLoader([url])
+    tt = Html2TextTransformer()
+    docs = tt.transform_documents(loader.load())
+
+    # 2. Split into chunks
+    ts = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=0)
+    fd = ts.split_documents(docs)
+
+    return fd[0].page_content
+
